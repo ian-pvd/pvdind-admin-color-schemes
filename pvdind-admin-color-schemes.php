f78b56817f206e4830d1d3eac46f56ed6716864d
Ian Del Giudice <ian.pvd@gmail.com>
2018-09-27 18:21:26 -0400

    WIP Industrial Theme

--- pvdind-admin-color-schemes.php
+++ pvdind-admin-color-schemes.php
@@ -39,8 +39,7 @@ class PVD_Color_Schemes {
 	 *                    Needed for registering colors-fresh dependency.
 	 */
 	private $colors = array(
-		'vinyard', 'primary', '80s-kid', 'aubergine',
-		'cruise', 'flat', 'lawn', 'seashore'
+		'industrial',
 	);
 
 	function __construct() {
@@ -54,59 +53,10 @@ class PVD_Color_Schemes {
 		$suffix = is_rtl() ? '-rtl' : '';
 
 		wp_admin_css_color(
-			'vinyard', __( 'Vinyard', 'pvd_colors' ),
-			plugins_url( "vineyard/colors$suffix.css", __FILE__ ),
-			array( '#301D25', '#462b36', '#d3995d', '#eabe3f' ),
+			'industrial', __( 'Industrial', 'pvd_colors' ),
+			plugins_url( "themes/industrial/colors$suffix.css", __FILE__ ),
+			array( '#1F2C39', '#242824', '#1abc9c', '#f39c12' ),
 			array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' )
-		);
-
-		wp_admin_css_color(
-			'primary', __( 'Primary', 'pvd_colors' ),
-			plugins_url( "primary/colors$suffix.css", __FILE__ ),
-			array( '#282b48', '#35395c', '#f38135', '#e7c03a' ),
-			array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' )
-		);
-
-		wp_admin_css_color(
-			'80s-kid', __( '80\'s Kid', 'pvd_colors' ),
-			plugins_url( "80s-kid/colors$suffix.css", __FILE__ ),
-			array( '#0A3D80', '#0c4da1', '#ed5793', '#eb853b' ),
-			array( 'base' => '#e4e5e7', 'focus' => '#fff', 'current' => '#fff' )
-		);
-
-		wp_admin_css_color(
-			'aubergine', __( 'Aubergine', 'pvd_colors' ),
-			plugins_url( "aubergine/colors$suffix.css", __FILE__ ),
-			array( '#4c4b5f', '#585a61', '#e87162', '#da9b49' ),
-			array( 'base' => '#e4e4e7', 'focus' => '#fff', 'current' => '#fff' )
-		);
-
-		wp_admin_css_color(
-			'cruise', __( 'Cruise', 'pvd_colors' ),
-			plugins_url( "cruise/colors$suffix.css", __FILE__ ),
-			array( '#292B46', '#36395c', '#8bbc9f', '#d2ac1f' ),
-			array( 'base' => '#f1f1f3', 'focus' => '#fff', 'current' => '#fff' )
-		);
-
-		wp_admin_css_color(
-			'flat', __( 'Flat', 'pvd_colors' ),
-			plugins_url( "flat/colors$suffix.css", __FILE__ ),
-			array( '#1F2C39', '#2c3e50', '#1abc9c', '#f39c12' ),
-			array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' )
-		);
-
-		wp_admin_css_color(
-			'lawn', __( 'Lawn', 'pvd_colors' ),
-			plugins_url( "lawn/colors$suffix.css", __FILE__ ),
-			array( '#0F1515', '#1e2a29', '#5D824B', '#a7b145' ),
-			array( 'base' => '#f1f3f3', 'focus' => '#fff', 'current' => '#fff' )
-		);
-
-		wp_admin_css_color(
-			'seashore', __( 'Seashore', 'pvd_colors' ),
-			plugins_url( "seashore/colors$suffix.css", __FILE__ ),
-			array( '#F8F6F1', '#d5cdad', '#7D6B5C', '#456a7f' ),
-			array( 'base' => '#533C2F', 'focus' => '#F8F6F1', 'current' => '#F8F6F1' )
 		);
 
 	}
