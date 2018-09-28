86b6d0cfabfe009c3abf2c8baa0c845111669a60
Ian Del Giudice <ian.pvd@gmail.com>
2018-09-28 18:50:17 -0400

    WIP Industrial Dark theme

---
+++ themes/industrial-dark/index.js
@@ -0,0 +1,8 @@
+/* Industrial Dark Theme JS */
+
+// Import Industrial Dark Styles
+import './industrial-dark.scss';
+
+// Make Changes to TinyMCE iFrame
+const theEditor = document.getElementById('content_ifr')
+  .contentWindow.document.getElementById('tinymce');
